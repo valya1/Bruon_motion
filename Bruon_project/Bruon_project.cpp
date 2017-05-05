d3a927e443a8fbc884542747ca06ffd85b446119
valya1 <strychkov@mail.ru>
2017-05-05 23:19:56 +0500

    Update Bruon_project.cpp

--- Bruon_project/Bruon_project.cpp
+++ Bruon_project/Bruon_project.cpp
@@ -1,32 +1,40 @@
-
+// Bruon_project.cpp: определяет точку входа для консольного приложения.
+//
 
 #include "stdafx.h"
 #include <glut.h>
 #include <vector>
-#include<ctime>
 #include<cmath>
-#include <omp.h>
-#include<iostream>
 #define M_PI   3.14159265358979323846
 #define DT 0.1
 using namespace std;
 
 struct Particle
 {
-	int num;
-	float r;
-	float m;
 	float x;
 	float y;
 	float vx;
 	float vy;
+	float m;
 };
 
 vector<Particle> p;
-
 float sqr(float x)
 {
 	return x*x;
+}
+void changeSize(int w, int h)
+{
+	if (h == 0)
+		h = 1;
+	float ratio = 1.0* w / h;
+
+	glMatrixMode(GL_PROJECTION);
+	glViewport(0, 0, w, h);
+
+	gluPerspective(45, ratio, 1, 1000);
+
+	glMatrixMode(GL_MODELVIEW);
 }
 void display()
 {
@@ -35,84 +43,83 @@ void display()
 	{
 		if (i.m <= 2)
 		{
-			glPointSize(6);
+			glPointSize(4);
 			glBegin(GL_POINTS);
 			glVertex2f(i.x, i.y);
 			glEnd();
 		}
 		else
 		{
-			glBegin(GL_POLYGON);
-			if (i.num == 1)
-				glColor3f(1,0,0);
-			else
-				glColor3f(0.1, 0.1, 1);
+			glBegin(GL_LINES);
 			for (int a = 0; a < 72; a++)
 			{
-				float x = i.r*cos(a*M_PI / 36);
-				float y = i.r*sin(a*M_PI / 36);
-				glVertex2f(i.x + x, i.y + y);
-				x = i.r*cos((a + 1)*M_PI / 36);
-				y = i.r*sin((a + 1)*M_PI / 36);
-				glVertex2f(i.x + x, i.y + y);
+				float x = i.m*cos(a*M_PI / 36);
+				float y = i.m*sin(a*M_PI / 36);
+				glVertex2f(i.x+x, i.y+y);
+				x = i.m*cos((a + 1)*M_PI / 36);
+				y = i.m*sin((a + 1)*M_PI / 36);
+				glVertex2f(i.x+x, i.y+y);
 			}
 			glEnd();
+			}
+
 		}
-	}
-	glutSwapBuffers();
+		glutSwapBuffers();
 }
 void timer(int = 0)
 {
-	float d;
-	for (int i = 0; i<p.size(); i++)
-		for (int j = 0; j<p.size(); j++)
-			if (i != j)
+	for (auto i = p.begin(); i!=p.end();i++)
+		for (auto j = p.begin(); j!=p.end();j++)
+			if (i!=j)
 			{
-				d = sqrt((p[i].x - p[j].x) * (p[i].x - p[j].x) + (p[i].y - p[j].y) * (p[i].y - p[j].y));
-				if (d < p[i].r + p[j].r)
+				float d = sqrt((i->x - j->x) * (i->x - j->x) + (i->y - j->y) * (i->y - j->y));
+				if (d < i->m + j->m)
 				{
-					float f = 1000 * (p[i].r + p[j].r - d);
-					/*float f = p[i].m*(abs(sqrt(p[i].vx*p[i].vx + p[i].vy*p[i].vy) - sqrt(p[j].vx*p[j].vx + p[j].vy*p[j].vy)));*/
-					p[i].vx += f * (p[i].x - p[j].x) / d / p[i].m * DT;
-					p[i].vy += f * (p[i].y - p[j].y) / d / p[i].m * DT;
-					p[j].vx -= f * (p[i].x - p[j].x) / d / p[j].m * DT;
-					p[j].vy -= f * (p[i].y - p[j].y) / d / p[j].m * DT;
+					float f = 50 * (i->m + j->m - d);
+					i->vx += f * (i->x - j->x) / d / i->m * DT;
+					i->vy += f * (i->y - j->y) / d / i->m * DT;
+					j->vx -= f * (i->x - j->x) / d / j->m * DT;
+					j->vy -= f * (i->y - j->y) / d / j->m * DT;
 				}
 			}
-	for (int i = 0; i < p.size(); ++i)
+	for (auto i = p.begin(); i != p.end(); ++i)
 	{
-		if ((p[i].x - p[i].r < 0) || (p[i].x + p[i].r >1366))
-			p[i].vx = -p[i].vx;
-		if ((p[i].y - p[i].r < 0) || (p[i].y + p[i].r >768))
-			p[i].vy = -p[i].vy;
-		p[i].x += p[i].vx * DT;
-		p[i].y += p[i].vy * DT;
+		i->x += i->vx * DT;
+		i->y += i->vy * DT;
+		if (i->x < 0)
+			i->x += 480;
+		if (i->y < 0)
+			i->y += 480;
+		if (i->x > 480)
+			i->x -= 480;
+		if (i->y > 480)
+			i->y -= 480;
 	}
 	display();
-	glutTimerFunc(7, timer, 0);
+	glutTimerFunc(10, timer, 0);
 }
 
 int main(int argc, char **argv)
 {
-	srand(time(NULL));
-	for (int i = 0; i < 200; i++)
+	for (int i = 0; i < 100; i++)
 	{
-		float m = 500;
-		float r = pow(m, 1.0 / 3.0);
-		Particle particle = { i, r, m, rand() % 1366, rand() % 768, rand() % 20500 / 500.0 - 10, rand() % 20500 / 500.0 - 10 };
+		Particle particle = { rand() % 480, rand() % 480, rand() % 10000 / 500.0 - 10, rand() % 10000 / 500.0 - 10, rand()%5 +1};
 		p.push_back(particle);
 	}
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(1366, 768);
+	glutInitWindowSize(480, 480);
+	glutInitWindowPosition(20, 1050 - 480 - 20);
 	glutCreateWindow("Brownian motion");
-	glClearColor(0.9, 0.9, 0.9, 0.9);
+	glClearColor(0, 0, 0, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0, 1366, 768, 0, -1, 1);
+	glOrtho(0, 480, 480, 0, -1, 1);
 	glutDisplayFunc(display);
 	timer();
 	glutMainLoop();
 	return 0;
 }
+
+
 
